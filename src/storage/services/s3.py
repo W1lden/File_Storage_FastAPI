5a0c1cca13857d71d27e89c6d2f728c8dd52b8bf
W1lden <Asan.Bitanov@gmail.com>
2025-09-09 18:33:34 +0500

    fix s3

--- src/storage/services/s3.py
+++ src/storage/services/s3.py
@@ -12,6 +12,6 @@ def get_client() -> Minio:
     return _client_internal
 
 def ensure_bucket() -> None:
-    client = get_internal_client()
+    client = get_client()
     if not client.bucket_exists(settings.MINIO_BUCKET_NAME):
         client.make_bucket(settings.MINIO_BUCKET_NAME)
