b66193fd60fd708534061d98413ac2dbbd873eff
W1lden <Asan.Bitanov@gmail.com>
2025-09-10 12:24:59 +0500

    update config and fix seed_admin

--- src/storage/scripts/seed_admin.py
+++ src/storage/scripts/seed_admin.py
@@ -1,28 +1,32 @@
 import asyncio
 from sqlalchemy import select
+from storage.core.config import settings
 from storage.core.db import async_session_maker
 from storage.core.security import get_password_hash
 from storage.db.models.user import User, UserRole
-from storage.core.config import settings
 
 
 async def main():
+    email = settings.ADMIN_EMAIL
+    password = settings.ADMIN_PASSWORD
+    department_id = (
+        int(settings.ADMIN_DEPARTMENT_ID) if settings.ADMIN_DEPARTMENT_ID else None
+    )
+
     async with async_session_maker() as session:
-        q = await session.execute(
-            select(User).where(User.email == settings["ADMIN_EMAIL"])
-        )
+        q = await session.execute(select(User).where(User.email == email))
         user = q.scalar_one_or_none()
         if user:
-            user.hashed_password = get_password_hash(
-                settings["ADMIN_PASSWORD"]
-            )
+            user.hashed_password = get_password_hash(password)
             user.role = UserRole.ADMIN
+            user.department_id = department_id
             user.is_active = True
         else:
             user = User(
-                email=settings["ADMIN_EMAIL"],
-                hashed_password=get_password_hash(settings["ADMIN_PASSWORD"]),
+                email=email,
+                hashed_password=get_password_hash(password),
                 role=UserRole.ADMIN,
+                department_id=department_id,
                 is_active=True,
             )
             session.add(user)
