3736ad2c5695b315a1d8e53c27922f69c87cb3b9
W1lden <Asan.Bitanov@gmail.com>
2025-09-10 11:49:53 +0500

    formate code

--- src/storage/scripts/seed_admin.py
+++ src/storage/scripts/seed_admin.py
@@ -3,20 +3,25 @@ from sqlalchemy import select
 from storage.core.db import async_session_maker
 from storage.core.security import get_password_hash
 from storage.db.models.user import User, UserRole
+from storage.core.config import settings
 
 
 async def main():
     async with async_session_maker() as session:
-        q = await session.execute(select(User).where(User.email == "admin@example.com"))
+        q = await session.execute(
+            select(User).where(User.email == settings["ADMIN_EMAIL"])
+        )
         user = q.scalar_one_or_none()
         if user:
-            user.hashed_password = get_password_hash("admin123")
+            user.hashed_password = get_password_hash(
+                settings["ADMIN_PASSWORD"]
+            )
             user.role = UserRole.ADMIN
             user.is_active = True
         else:
             user = User(
-                email="admin@example.com",
-                hashed_password=get_password_hash("admin123"),
+                email=settings["ADMIN_EMAIL"],
+                hashed_password=get_password_hash(settings["ADMIN_PASSWORD"]),
                 role=UserRole.ADMIN,
                 is_active=True,
             )
